d84560b5f264b90c87ede8ba7590ea88bb1f521b
pop3il <b.pasquier69@gmail.com>
2015-03-24 22:10:02 +0100

    BP: First commit

---
+++ TaskApp/TaskApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaskApp
+//
+//  Created by Benoit PASQUIER on 24/03/2015.
+//  Copyright (c) 2015 Benoit Pasquier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaskApp/TaskApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaskApp
+//
+//  Created by Benoit PASQUIER on 24/03/2015.
+//  Copyright (c) 2015 Benoit Pasquier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
